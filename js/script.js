834192a4d16f698961ce171fde573bb3e1c2d51b
RiccardoBasso01 <riccardo.basso2001@gmail.com>
2023-05-30 15:09:30 +0200

    Trascrizione dati in pagina

--- js/script.js
+++ js/script.js
@@ -9,12 +9,16 @@ console.log(element);
 for(let i = 1; i <= 100; i++){
     if(!(i % 3) && !(i % 5)){
         console.log('FizzBuzz')
+        element.innerHTML += `<div class="FizzBuzz">FizzBuzz</div>`
     }else if(!(i % 3)){
         console.log('Fizz')
+        element.innerHTML += `<div class="Fizz">Fizz</div>`
     }else if(!(i % 5)){
         console.log('Buzz')
+        element.innerHTML += `<div class="Buzz">Buzz</div>`
     }else{
         console.log(i)
+        element.innerHTML += `<div>${i}</div>`
     }
 }
 
