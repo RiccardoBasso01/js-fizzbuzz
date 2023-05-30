6374c359436847f21c4425943be430fea32ecf45
RiccardoBasso01 <riccardo.basso2001@gmail.com>
2023-05-30 14:55:04 +0200

    Esercizio senza bonus

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 console.log('JS OK')
+
+
+// Recupero l'elemento dal DOM
+const element = document.getElementById('element');
+console.log(element);
+
+//Generare umeri da 1 a 100 assegnandoli il giusto valore
+for(let i = 1; i <= 100; i++){
+    if(!(i % 3) && !(i % 5)){
+        console.log('FizzBuzz')
+    }else if(!(i % 3)){
+        console.log('Fizz')
+    }else if(!(i % 5)){
+        console.log('Buzz')
+    }else{
+        console.log(i)
+    }
+}
+
